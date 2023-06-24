ab7b854a5ab889cb558328b40019f79bd338212c
matsaina <matsaina@gmail.com>
2023-06-24 20:49:43 +0300

    complete

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,8 +6,14 @@ class ToysController < ApplicationController
     render json: toys
   end
 
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy, status: :ok
+  end
+
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :toys, only: [:index, :create, :update]
+  
+  resources :toys, only: [:index, :create, :update, :show, :destroy]
+
+
 end
